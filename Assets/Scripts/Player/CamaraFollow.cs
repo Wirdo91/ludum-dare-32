cf31bc3dbb150a7bc321240f8f7df5fcf6437523
jackie.engberg@get2net.dk <Jackie Christensen>
2015-04-19 06:11:03 +0200

    Minimap

---
+++ Assets/Scripts/Player/CamaraFollow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamaraFollow : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+	
+	void Update ()
+    {
+        this.transform.position = target.position + (Vector3.back * 10);
+	}
+}
