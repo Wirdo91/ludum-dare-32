63fdd364ba6b851a0532174a211d88ae861e822e
jackie.engberg@get2net.dk <Jackie Christensen>
2015-04-20 16:37:16 +0200

    Explosive Shart Gun

--- Assets/Scripts/Bullet/ShartBullet.cs
+++ Assets/Scripts/Bullet/ShartBullet.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class ShartBullet : Bullet
 {
+    float ExplosionTime = 0.5f;
     // Use this for initialization
     public override void Start()
     {
@@ -12,6 +13,24 @@ public class ShartBullet : Bullet
     // Update is called once per frame
     public override void Update()
     {
-        base.Update();
+        if (lifeTime >= lifeSpan)
+        {
+            if (lifeTime >= lifeSpan + ExplosionTime)
+            {
+                DestroyObject();
+                return;
+            }
+            this.GetComponent<CircleCollider2D>().radius = 2;
+            this.GetComponent<SpriteRenderer>().sprite = bulletSprites[Random.Range(1, bulletSprites.Length)];
+            this.GetComponent<SpriteRenderer>().color = new Color32(180, 60, 10, 255);
+            this.transform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            transform.Translate(-Vector2.right * Time.deltaTime * speed, Space.Self);
+            this.GetComponent<SpriteRenderer>().sprite = bulletSprites[0];
+            this.GetComponent<SpriteRenderer>().color = new Color32(60, 30, 0, 255);
+        }
+        lifeTime += Time.deltaTime;
     }
 }
